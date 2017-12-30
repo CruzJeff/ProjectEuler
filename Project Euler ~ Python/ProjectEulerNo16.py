a605d91a00260beb8e008680dadfa5220aaecc7e
CruzJeff <26846343+CruzJeff@users.noreply.github.com>
2017-12-30 18:42:04 -0500

    Solved 16,17,18

---
+++ Project Euler ~ Python/ProjectEulerNo16.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 02:06:13 2017
+
+@author: User
+"""
+
+result = 0
+
+for char in str(2**1000):
+    result = result + int(char)
+    
+print(result)
---
+++ Project Euler ~ Python/ProjectEulerNo17.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 09:55:18 2017
+
+@author: User
+"""
+
+"Number 17 was solved without a computer"
+
+    
